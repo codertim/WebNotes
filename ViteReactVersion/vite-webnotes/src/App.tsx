95de6c8129659e0ca2e0d2b397f33e537ee34da0
Tim Force <twforce@yahoo.com>
2024-04-05 16:03:39 -0700

    added sass

--- ViteReactVersion/vite-webnotes/src/App.tsx
+++ ViteReactVersion/vite-webnotes/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './myscss/main.scss'
 import Notes from './components/Notes.tsx'
 
 function App() {
