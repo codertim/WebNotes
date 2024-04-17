06b29741f88d9b83b6414d3ddda2ce43a3962f7b
Tim Force <twforce@yahoo.com>
2024-04-16 19:03:36 -0700

    experimenting with css clamp

--- ViteReactVersion/vite-webnotes/src/App.tsx
+++ ViteReactVersion/vite-webnotes/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <div id="initial-header">Vite + React</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
