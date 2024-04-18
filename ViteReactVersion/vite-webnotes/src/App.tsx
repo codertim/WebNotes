67be8025ed308c30ca59ce70e651fb74cee10d54
Tim Force <twforce@yahoo.com>
2024-04-17 17:12:50 -0700

    removed default text

--- ViteReactVersion/vite-webnotes/src/App.tsx
+++ ViteReactVersion/vite-webnotes/src/App.tsx
@@ -19,19 +19,6 @@ function App() {
         </a>
       </div>
       <div id="initial-header">Vite + React</div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-
 
       <main>
       <article>
