483052c8ac9560b9bef7a8b09eb33ac792dfd06a
Tim Force <twforce@yahoo.com>
2024-06-12 16:51:01 -0700

    added hover styles over text area

--- ViteReactVersion/vite-webnotes/src/components/Notes.tsx
+++ ViteReactVersion/vite-webnotes/src/components/Notes.tsx
@@ -277,7 +277,7 @@ function Notes() {
                 <textarea id="msg-text-area" ref={_textAreaEl} rows={10} cols={50}
                           style={ {backgroundColor: '#72bcd4', caretColor: "yellow"} }
                           defaultValue={initialNotes}
-                          className="shadow rounded font-semibold border-2 leading-6 p-1 md:p-4"
+                          className="shadow rounded font-semibold border-2 leading-6 p-1 md:p-4 hover:border-solid hover:border-blue-500"
                           ></textarea>
                 <div>
                     <button type="button" style={{margin: '1rem'}} className="hover:bg-blue-400 border-slate-200 h-10 px-6 bg-orange shadow-sm rounded-lg text-red w-56" onClick={updateNotes}>Update</button>
